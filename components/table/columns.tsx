46645665850b3c3a66daa3637dbf1323ca81d89a
LorenaGrasu13 <lorenagrasu.13@gmail.com>
2024-08-20 13:10:37 +0300

    Admin page

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -7,8 +7,8 @@ import { Doctors } from "@/constants";
 import { formatDateTime } from "@/lib/utils";
 import { Appointment } from "@/types/appwrite.types";
 
-import { AppointmentModal } from "../AppointmentModal";
-import { StatusBadge } from "../StatusBadge";
+import StatusBadge from "../StatusBadge";
+import AppointmentModal from "../AppointmentModal";
 
 export const columns: ColumnDef<Appointment>[] = [
   {
@@ -86,16 +86,16 @@ export const columns: ColumnDef<Appointment>[] = [
             userId={appointment.userId}
             appointment={appointment}
             type="schedule"
-            title="Schedule Appointment"
-            description="Please confirm the following details to schedule."
+            //title="Schedule Appointment"
+            //description="Please confirm the following details to schedule."
           />
           <AppointmentModal
             patientId={appointment.patient.$id}
             userId={appointment.userId}
             appointment={appointment}
             type="cancel"
-            title="Cancel Appointment"
-            description="Are you sure you want to cancel your appointment?"
+            //title="Cancel Appointment"
+            //description="Are you sure you want to cancel your appointment?"
           />
         </div>
       );
